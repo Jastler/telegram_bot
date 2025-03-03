8b090e349cfdeba1607ac3f02dc0574558f78694
Taras <chirkovtaras@gmail.com>
2025-03-03 22:59:32 +0200

    Revert "feat: add stars"
    This reverts commit c47d876422a1fb5be136bc1d0ed1bc65eca51901.

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
+const { setupPayments } = require("./payments"); // Підключаємо payments.js
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
